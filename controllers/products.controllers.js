318d8964d1f856e53559d41bb3521c8ad85d0ece
Bianca Karitas jensen <5884546@rts-365.dk>
2019-11-12 12:26:38 +0100

    Update

--- controllers/products.controllers.js
+++ controllers/products.controllers.js
@@ -1,15 +1,12 @@
 const productRef = require("../models/product.model");
 
-const { log } = require("./middleware/logger");
+const { log } = require("../middleware/logger");
 
-function getSingleProduct(docs) {
-	return docs[0];
-};
 
 exports.getSingleProduct = async function (req, res){
 	try {
 		const docs = await productRef
-		.where("sku", "=", req.params.sku)
+		.where("sku", "==", req.params.sku)
 		.limit(1)
 		.get()
 	docs.forEach(doc => res.json(doc.data()));
@@ -18,11 +15,28 @@ exports.getSingleProduct = async function (req, res){
 		res.status(500).end();
 	}
 };
+/* exports.getSingleProduct = function (req, res){
+	.then(docs => {
+		productRef.where("sku", "=", req.params.sku).get()
+			docs.forEach(doc => res.json(doc.data()))
+		});
+}; */
 
-exports.createProduct = function (req, res){//Hardcoder databasen. Henter createProduct fra home.rute.js filen.
+exports.createProduct = async function (req, res){
+	try {
+		req.fields.price = parseFloat(req.fields.price);//Skrives kun ved price og weight fordi de er numbers og ikke strings.
+		req.fields.weight = parseFloat(req.fields.weight);
+		const ref = await productRef.add({ ...req.fields })
+		const doc = await ref.get();
+		res.status(201).json(doc.data())
+	}catch (error) {
+		log.error(error.stack);
+		res.status(500).end();
+	}
+	
+};
+/* exports.createProduct = function (req, res){//Hardcoder databasen. Henter createProduct fra home.rute.js filen.
 	//Opret data.
-	req.fields.price = parseFloat(req.fields.price);//Skrives kun ved price og weight fordi de er numbers og ikke strings.
-	req.fields.weight = parseFloat(req.fields.weight);
 	productRef.add({ ...req.fields })//det samme som man skriver obejkter med værdier. altså .... name: "Bjarne", price: 100 osv....
 		.then(ref => {//Udskriver json - data'en.
 			console.log("hej")
@@ -31,22 +45,27 @@ exports.createProduct = function (req, res){//Hardcoder databasen. Henter create
 				.catch(err => console.log(err))
 		})
 		.catch(error => res.json(error));//Hvis der sker en fejl skriver den fejlen ud stedenfor json - data'en.
-};
+}; */
 
-exports.getAllProducts = function(req, res){
+exports.getAllProducts = async function (req, res){
+	try{
+		const docs = await productRef.get();
+		const results = [];
+		docs.forEach(doc => results.push(doc.data()));
+		res.json(results);
+	}catch (error){
+		log.error(error.stack);
+		res.status(500).end();
+	}
+};
+/* exports.getAllProducts = function(req, res){
 	productRef.get().then(docs => {
 		const result = []; //Laver et tomt array til at putte documenterne i.
 		docs.forEach(doc => result.push(doc.data()))
 		res.json(result);
 	});
-};
+}; */
 
-exports.getSingleProduct = function (req, res){
-	productRef.where("sku", "=", req.params.sku).get()
-		.then(docs => {
-			docs.forEach(doc => res.json(doc.data()))
-		});
-};
  
 exports.updateProduct = function (req, res){
 	if (req.fields.price){
@@ -64,12 +83,22 @@ exports.updateProduct = function (req, res){
 		})
 };
 
-exports.deleteProduct = function (req, res){
-	productRef.where("sku", "=", req.params.sku).get() //Leder efter et sku er lig det det sku, der er.
+exports.deleteProduct = async function (req, res){
+	try{
+		const docs = await productRef.where("sku", "==", req.params.sku).get()
+		docs.forEach(doc => doc.ref.delete());
+		res.status(204).end();
+	}catch (error){
+		res.status(500).end();
+		log.error(error.stack);
+	}
+};
+/* exports.deleteProduct = function (req, res){
+	productRef.where("sku", "==", req.params.sku).get() //Leder efter et sku er lig det det sku, der er.
 		.then(docs => {//så tager man sku'en
-			docs.forEach(doc => doc.delete());//For hvert document som der er sletter man.
+			docs.forEach(doc => doc.ref.delete());//For hvert document som der er sletter man.
 		})
 		.catch(error => res.status(5000).json({message: error}));//Hvis der sker en fejl skriver den fejlen ud stedenfor json - data'en.
 	res.status(204).end();//Skriver den - "Alt er iorden, men der er ikke noget document" at vise, fordi det lige er blevet slettet.
-};
+}; */
 
